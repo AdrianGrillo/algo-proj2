941d67391ad65badec591d3f4603491b4552f89d
Adrian <grillo.adrian13@gmail.com>
2022-11-14 19:17:29 -0500

    store parts and dependencies

--- main.cpp
+++ main.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <regex>
 
 using namespace std;
 
 int main() {
 
-    cout << "blank slate" << endl;
+	// Open input file
+	ifstream input_file;
 
-    return 0;
+	input_file.open("input.txt");
+
+	if(!input_file) {
+		cout << "Unable to open input file" << endl;	
+		return 1;
+	}
+
+	string line;
+	int parts, dependencies;
+	regex regex("\\d\\s\\d");
+	cmatch match;
+
+	// Store the number of parts and dependencies
+	while(getline(input_file, line)) {
+		if(regex_match(line.c_str(), match, regex)) {
+			parts = stoi(line.substr(0, 1));
+			dependencies = stoi(line.substr(1, 2));
+			cout << parts << endl;
+			cout << dependencies << endl;
+			break;
+		}
+	}
+
+	return 0;
 }
