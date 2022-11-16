b2625c4b82d4cb0d3817eb7662e8363075c8c810
Adrian <grillo.adrian13@gmail.com>
2022-11-15 20:12:55 -0500

    fixed regex bug

--- main.cpp
+++ main.cpp
@@ -19,14 +19,18 @@ int main() {
 
 	string line;
 	int parts, dependencies;
-	regex regex("\\d\\s\\d");
+	regex regex("\\d*\\s\\d*");
 	cmatch match;
 
 	// Store the number of parts and dependencies
 	while(getline(input_file, line)) {
 		if(regex_match(line.c_str(), match, regex)) {
-			parts = stoi(line.substr(0, 1));
-			dependencies = stoi(line.substr(1, 2));
+			int whitespace = line.find(" ");
+			int n = line.length();
+
+			parts = stoi(line.substr(0, whitespace));
+			dependencies = stoi(line.substr(whitespace + 1, n - whitespace - 1));
+
 			cout << parts << endl;
 			cout << dependencies << endl;
 			break;
