780fc9c3d279b87920fff865c9c750feebb1076e
Adrian <grillo.adrian13@gmail.com>
2022-11-15 23:56:34 -0500

    lookup array for memoization

--- main.cpp
+++ main.cpp
@@ -17,8 +17,8 @@ int main() {
 		return 1;
 	}
 
-	string line;
-	int parts, dependencies;
+	string line = "";
+	int n, m;
 	regex regex("\\d*\\s\\d*");
 	cmatch match;
 
@@ -28,13 +28,43 @@ int main() {
 			int whitespace = line.find(" ");
 			int n = line.length();
 
-			parts = stoi(line.substr(0, whitespace));
-			dependencies = stoi(line.substr(whitespace + 1, n - whitespace - 1));
+			n = stoi(line.substr(0, whitespace));
+			m = stoi(line.substr(whitespace + 1, n - whitespace - 1));
 
-			cout << parts << endl;
-			cout << dependencies << endl;
+			// cout << parts << endl;
+			// cout << dependencies << endl;
 			break;
 		}
+	}
+
+	// Store basic and intermediate parts in lookup array
+	line = "";
+	int line_number = 0;
+	bool start = false;
+	vector<int> lookup(m - 1);
+
+	while(getline(input_file, line) && line_number < m) {
+		// Iterate to two lines after the string
+		if(start != true && regex_match(line.c_str(), match, regex))
+			start = true;
+
+		if(start == true) {
+			++line_number;
+			// Get basic and intermediate parts
+			int whitespace = line.find(" ");
+			int n = line.length();
+
+			int basic_part = stoi(line.substr(0, whitespace));
+			int intermediate_part = stoi(line.substr(whitespace + 1, n - whitespace - 1));
+			
+			// Part number = index, sprocket cost = value at that index
+			lookup[basic_part] = intermediate_part;
+		}
+
+	}
+
+	for(int i = 0; i < lookup.size(); ++i) {
+		cout << "Value at index "<< i <<" : " << lookup[i] << endl;
 	}
 
 	return 0;
