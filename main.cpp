2d9ae1b876ac0d3acbc5fb9fa8a38eb71fb5926d
Adrian <grillo.adrian13@gmail.com>
2022-11-17 17:20:53 -0500

    cleaned everything up for group mates

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main()
 	regex pair("\\d*\\s\\d*");
 	cmatch match;
 
-	// Store the number of dependencies and parts
+	// Get n and m
 	while(getline(input_file, line)) {
 		if(regex_match(line.c_str(), match, pair)) {
 			int whitespace = line.find(" ");
@@ -43,9 +43,44 @@ int main()
 		}
 	}
 
-	// Store sprocket cost for each part in sprocketCounts array
+	// Iterate through integer pairs - Store part number at that index with sprocket cost as value at index
 	int line_number = 0;
 	bool start = false;
+	vector<vector<int>> assembly_list(n - 1);
+
+	// Iterate to line where n and m are declared and start on next line
+	while(getline(input_file, line) && line_number < m) {
+		if(start != true && regex_match(line.c_str(), match, pair)) {
+			start = true;
+			continue;
+		}
+
+		if(start == true) {
+			// Get basic and intermediate parts
+			int whitespace = line.find(" ");
+			int length = line.length();
+
+			int basic_part = getSubstring(line, 0, whitespace);
+			int intermediate_part = getSubstring(line, whitespace + 1, length - whitespace - 1);
+			
+			assembly_list[basic_part].push_back(intermediate_part);
+			++line_number;
+		}
+	}
+
+	// Print 2d vector for debugging
+	cout << "Constructed from int pairs" << endl;
+	for(int i = 0; i < assembly_list.size(); ++i) {
+		cout << "Index " << i << ": ";
+		for(int j = 0; j < assembly_list[i].size(); j++) {
+			cout << assembly_list[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	// Iterate through single integers - Store sprocket cost for each part in sprocketCounts array
+	line_number = 0;
+	start = false;
 	vector<int> sprocketCounts(n);
 	regex single("\\d*");
 
@@ -57,58 +92,14 @@ int main()
 		// Iterate for as long as there is a single digit on a given line
 		if(start == true) {
 			sprocketCounts[line_number] = stoi(line);
-
 			++line_number;
 		}
 	}
 
-	// for(int i = 0; i < sprocketCounts.size(); ++i) {
-	// 	cout << "Index "<< i << ": " << sprocketCounts[i] << endl;
-	// }
-
-	// --------------------------------------------------------------------------------------------------------------------------
-
-	// Return to beginning of input file
-	input_file.clear();
-	input_file.seekg(0, ios::beg);
-
-	// Store part number at that index with sprocket cost as value at index
-	line_number = 0;
-	start = false;
-	vector<int> basic_parts(n - 1);
-	regex lit("\\d*");
-
-	cout << "hey" << endl;
-
-	// Iterate to line where singles start and store them
-	while(getline(input_file, line)) {
-		if(start != true && regex_match(line.c_str(), match, lit)) {
-			start = true;
-		}
-
-		cout << start <<endl;
-
-		if(start == true) {
-			cout << "hi" << endl;
-			// Get basic and intermediate parts
-			int whitespace = line.find(" ");
-			int length = line.length();
-
-			int basic_part = getSubstring(line, 0, whitespace);
-			// int intermediate_part = getSubstring(line, whitespace + 1, length - whitespace - 1);
-
-			// lookupAssembly[basic_part] = intermediate_part;
-
-			basic_parts[basic_part] = 1;
-
-			cout << basic_part;
-
-			++line_number;
-		}
-	}
-
-	for(int i = 0; i < basic_parts.size(); ++i) {
-		cout << "Index "<< i << ": " << basic_parts[i] << endl;
+	cout << endl;
+	cout << "Contructed from single ints: " << endl;
+	for(int i = 0; i < sprocketCounts.size(); ++i) {
+		cout << "Index "<< i << ": " << sprocketCounts[i] << endl;
 	}
 
 	input_file.close();
