e879d7afa935c456cffbeabc9d50d7f17c099e34
Adrian <grillo.adrian13@gmail.com>
2022-11-20 16:42:16 -0500

    save point

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
 #include <utility>
 #include <vector>
+#include <istream>
+#include <sstream>
 
 using namespace std;
 
-// Omnidroid process
-// 1. Extract n and m
-// 2. Extract pairs to 2d vector
-// 3. Extract singles to 1d vector
-// 4. Loop through 2d vector and reference 1d vector to calculate sprocket cost
-
-// Global dependency cost table
-vector<int> dependency_cost_total;
+// Global for total number of robots in file
+int totalRobots;
 
 // Get substring and return as int
 int getSubstring(string line, int start, int end) 
@@ -25,13 +20,14 @@ int getSubstring(string line, int start, int end)
 void getPartsAndDependencies(ifstream& file, int* n, int* m)
 {
 	string line = "";
-	regex pair("\\d*\\s\\d*");
-	cmatch match;
 
-	// Get n and m
 	while(getline(file, line)) 
 	{
-		if(regex_match(line.c_str(), match, pair)) 
+        if(!totalRobots)
+            totalRobots = stoi(line);
+
+        cout << "Total robots: " << totalRobots << endl;
+		if(line.length() >= 3 && line != "omnidroid" && line != "robotomaton") 
 		{
 			int whitespace = line.find(" ");
 			int length = line.length();
@@ -44,111 +40,65 @@ void getPartsAndDependencies(ifstream& file, int* n, int* m)
 	}
 }
 
-// Create 2d vector out of int pairs
-vector<vector<int>> constructAssemblyList(ifstream& file, vector<int> part_cost_individual, int m, int n) 
+void getAssemblyAndPartsList(ifstream& file, int n, int m,  vector<int>& part_cost, vector<vector<int>>& assembly_list) 
 {
-	// Iterate through integer pairs - Store part number at that index with sprocket cost as value at index
-	string line = "";
+    // Initialize each vector at result[i] -1, this indicates that part #i isn't dependent on any other part
+	for(int i = 0; i < assembly_list.size(); ++i){
+		assembly_list[i].push_back(-1);
+		// dependency_cost_total.push_back(0);
+	}
+
+    string line = "";
 	int line_number = 0;
-	bool start = false;
-	vector<vector<int>> result(n);
-	cmatch match;
-	regex pair("\\d\\s\\d");
 
-	// Initialize nested vectors to -1 and global lookup table to 0
-	for(int i = 0; i < result.size(); ++i){
-		// -1 indicates that part i isn't dependent on any other part
-		result[i].push_back(-1);
-		dependency_cost_total.push_back(0);
+    while(getline(file, line)) 
+    {
+        if(line.length() >= 3)
+        {
+        // Get basic and intermediate parts
+        int whitespace = line.find(" ");
+        int length = line.length();
+
+        int basic_part = getSubstring(line, 0, whitespace);
+        int intermediate_part = getSubstring(line, whitespace + 1, length - whitespace - 1);
+
+        // If -1 is at the location that a dependency shoud go, clear it before pushing the dependency
+        if(assembly_list[intermediate_part][0] == -1)
+            assembly_list[intermediate_part].clear();
+
+        assembly_list[intermediate_part].push_back(basic_part);
+        // dependency_cost_total[intermediate_part] += part_cost_individual[basic_part];
+        } 
+        else
+        {
+            ++line_number;
+            part_cost[line_number] = stoi(line);
+        }
 	}
-
-	// Iterate to line where n and m are declared and start on next line
-	while(getline(file, line)) {
-		if(start != true && regex_match(line.c_str(), match, pair))
-			start = true;
-
-		if(start == true) 
-		{
-			++line_number;
-			if(line_number > m)
-				break;
-
-			// Get basic and intermediate parts
-			int whitespace = line.find(" ");
-			int length = line.length();
-
-			int basic_part = getSubstring(line, 0, whitespace);
-			int intermediate_part = getSubstring(line, whitespace + 1, length - whitespace - 1);
-
-			// If -1 is at the location that a dependency shoud go, clear it before pushing the dependency
-			if(result[intermediate_part][0] == -1)
-				result[intermediate_part].clear();
-
-			result[intermediate_part].push_back(basic_part);
-			dependency_cost_total[intermediate_part] += part_cost_individual[basic_part];
-		}
-	}
-
-	for(int i = 0; i < dependency_cost_total.size(); ++i)
-		cout << dependency_cost_total[i] << endl;
-
-	return result;
 }
 
-// Index = part number, value @ index = part cost
-vector<int> getSingles(ifstream& file, int n)
-{
-	// Iterate through single integers - Store sprocket cost for each part in part_cost_individual array
-	string line = "";
-	int line_number = 0;
-	bool start = false;
-	vector<int> result(n);
-	cmatch match;
-	regex single("\\d*"), pair("\\d\\s\\d");
+// vector<int> getPartCost(ifstream& file, int n)
+// {
+// 	string line = "";
+// 	int line_number = 0;
+// 	vector<int> result(n);
 
-	// Iterate past first single digit since it has nothing to do with omnidroid assembly or else regex will match it
-	while(getline(file, line)) 
-	{
-		// Iterate to single ints
-		if(start != true && regex_match(line.c_str(), match, pair))
-			break;
-	}
+// 	while(getline(file, line) && line_number < n) 
+// 	{
+//         if(line.find(" ") == -1 && line != "omnidroid" && line != "robotomaton" && line != "")
+//         {
+//             result[line_number] = stoi(line);
+//             ++line_number;
+//         }
+// 	}
 
-	while(getline(file, line) && line_number < n) 
-	{
-		// Iterate to single ints
-		if(start != true && regex_match(line.c_str(), match, single))
-			start = true;
-
-		// Iterate for as long as there is a single digit on a given line
-		if(start == true) {
-			result[line_number] = stoi(line);
-			++line_number;
-		}
-	}
-
-	return result;
-}
-
-// Actual process of constructing robot from extracted pairs and singles
-int constructOmnidroid(vector<vector<int>> assembly, vector<int> part_cost_individual)
-{
-	int result = 0, n = assembly.size();
-
-	// Iterate through last index of dependency_cost_total and compute omnidroid cost
-	for(int i = 0; i < assembly[n - 1].size(); ++i)
-		result += dependency_cost_total[assembly[n - 1][i]] + part_cost_individual[assembly[n - 1][i]];
-
-	result += part_cost_individual[n - 1];
-	
-	return result;
-}
+// 	return result;
+// }
 
 int main() 
 {
-	// Open input file
+    // Open input file
 	ifstream input_file;
-
 	input_file.open("input.txt");
 
 	if(!input_file) 
@@ -157,20 +107,14 @@ int main()
 		return 1;
 	}
 
+    // Get n and m
 	int n, m;
-
 	getPartsAndDependencies(input_file, &n, &m);
 
-	vector<int> part_cost_individual = getSingles(input_file, n);
+    vector<int> part_cost(n);
+    vector<vector<int>> assembly_list(n);
+    getAssemblyAndPartsList(input_file, n, m, part_cost, assembly_list);
 
-	// Seek to beginning of input file
-	input_file.clear();
-	input_file.seekg(0);
-
-	vector<vector<int>> assembly_list = constructAssemblyList(input_file, part_cost_individual, m, n);
-
-	// Print 2d vector for debugging
-	cout << "Constructed from int pairs" << endl;
 	for(int i = 0; i < assembly_list.size(); ++i) 
 	{
 		cout << "Index " << i << ": ";
@@ -179,17 +123,7 @@ int main()
 		cout << endl;
 	}
 
-	// Print singles
-	cout << endl;
-	cout << "Contructed from single ints: " << endl;
-	for(int i = 0; i < part_cost_individual.size(); ++i)
-		cout << "Index "<< i << ": " << part_cost_individual[i] << endl;
 
-	int omnidroidCost = constructOmnidroid(assembly_list, part_cost_individual);
-	cout << endl;
-	cout << "Total omnidroid cost: " << omnidroidCost << endl;
-
-	input_file.close();
-
-	return 0;
+    for(int i = 0; i < part_cost.size(); ++i)
+		cout << "Index "<< i << ": " << part_cost[i] << endl;
 }
