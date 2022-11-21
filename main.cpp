57dc19668a9a7e3d146ea1d821c4d7a2e5812cae
Adrian <grillo.adrian13@gmail.com>
2022-11-20 21:19:54 -0500

    maybe now

--- main.cpp
+++ main.cpp
@@ -32,52 +32,52 @@ void getParts(ifstream& file, int* n)
 
 void getAssemblyAndPartsList(ifstream& file, int n,  vector<int>& part_cost, vector<vector<int>>& assembly_list) 
 {
-    // Initialize each vector at result[i] -1, this indicates that part #i isn't dependent on any other part
-	for(int i = 0; i < assembly_list.size(); ++i)
-		assembly_list[i].push_back(-1);
+	// Initialize each vector at result[i] -1, this indicates that part #i isn't dependent on any other part
+for(int i = 0; i < assembly_list.size(); ++i)
+	assembly_list[i].push_back(-1);
 
-    string line = "";
+	string line = "";
 	int line_number = 0;
 
-    while(getline(file, line)) 
-    {
-        if(line.length() >= 3)
-        {
-        // Get basic and intermediate parts
-        int whitespace = line.find(" ");
-        int length = line.length();
+	while(getline(file, line)) 
+	{
+		if(line.length() >= 3)
+		{
+			// Get basic and intermediate parts
+			int whitespace = line.find(" ");
+			int length = line.length();
 
-        int basic_part = stoi(line.substr(0, whitespace));;
-        int intermediate_part = stoi(line.substr(whitespace + 1, length - whitespace + 1));
+			int basic_part = stoi(line.substr(0, whitespace));;
+			int intermediate_part = stoi(line.substr(whitespace + 1, length - whitespace + 1));
 
-        // If -1 is at the location that a dependency shoud go, clear it before pushing the dependency
-        if(assembly_list[intermediate_part][0] == -1)
-            assembly_list[intermediate_part].clear();
+			// If -1 is at the location that a dependency shoud go, clear it before pushing the dependency
+			if(assembly_list[intermediate_part][0] == -1)
+				assembly_list[intermediate_part].clear();
 
-        assembly_list[intermediate_part].push_back(basic_part);
-        } 
-        else
-        {
-            part_cost[line_number] = stoi(line);
-            ++line_number;
-        }
+			assembly_list[intermediate_part].push_back(basic_part);
+			} 
+			else
+			{
+				part_cost[line_number] = stoi(line);
+				++line_number;
+		}
 	}
 }
 
 void getDependencyCost(const vector<vector<int>>& assembly_list, const vector<int>& part_cost, vector<int>& dependency_cost)
 {
-    // Initialize depency_cost vector to 0
-    for(int i = 0; i < assembly_list.size(); ++i)
-        dependency_cost.push_back(0);
+	// Initialize depency_cost vector to 0
+	for(int i = 0; i < assembly_list.size(); ++i)
+		dependency_cost.push_back(0);
 
-    for(int i = 0; i < assembly_list.size(); ++i) 
-    {
-        if(assembly_list[i][0] != -1) 
-        {
-            for(int j = 0; j < assembly_list[i].size(); ++j)
-                dependency_cost[i] += part_cost[assembly_list[i][j]];
-        }
-    }
+	for(int i = 0; i < assembly_list.size(); ++i) 
+	{
+		if(assembly_list[i][0] != -1) 
+		{
+			for(int j = 0; j < assembly_list[i].size(); ++j)
+				dependency_cost[i] += part_cost[assembly_list[i][j]];
+		}
+	}
 }
 
 int constructOmnidroid(const vector<vector<int>> assembly_list, const vector<int> part_cost, const vector<int> dependency_cost)
@@ -95,7 +95,7 @@ int constructOmnidroid(const vector<vector<int>> assembly_list, const vector<int
 
 int main() 
 {
-    // Open input file
+	// Open input file
 	ifstream input_file;
 	input_file.open("input.txt");
 
@@ -105,26 +105,26 @@ int main()
 		return 1;
 	}
 
-    // Get n
+	// Get n
 	int n;
 	getParts(input_file, &n);
 
-    // Index = part number, value @ index = part cost
-    vector<int> part_cost(n);
+	// Index = part number, value @ index = part cost
+	vector<int> part_cost(n);
 
-    // Index = part number, ints @ index = dependencies
-    vector<vector<int>> assembly_list(n);
-    getAssemblyAndPartsList(input_file, n, part_cost, assembly_list);
+	// Index = part number, ints @ index = dependencies
+	vector<vector<int>> assembly_list(n);
+	getAssemblyAndPartsList(input_file, n, part_cost, assembly_list);
 
-    // Index = part number, value @ index = cost of all dependencies
-    vector<int> dependency_cost;
-    getDependencyCost(assembly_list, part_cost, dependency_cost);
+	// Index = part number, value @ index = cost of all dependencies
+	vector<int> dependency_cost;
+	getDependencyCost(assembly_list, part_cost, dependency_cost);
 
-    int omnidroidCost = constructOmnidroid(assembly_list, part_cost, dependency_cost);
+	int omnidroidCost = constructOmnidroid(assembly_list, part_cost, dependency_cost);
 
-    cout << "Omnidroid cost: " << omnidroidCost << endl;
+	cout << "Omnidroid cost: " << omnidroidCost << endl;
 
-    input_file.close();
+	input_file.close();
 
-    return 0;
+	return 0;
 }
